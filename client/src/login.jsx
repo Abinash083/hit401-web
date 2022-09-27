b53e8f2551133f26e7ee55fac0f988a1f1b71fb1
Abinash <avinashniure026@gmail.com>
2022-09-27 14:08:31 +0930

    log in for all user combine

---
+++ client/src/login.jsx
@@ -0,0 +1,29 @@
+import {userState} from "react";
+
+
+const Login_page = () => {
+    /**const [email,setEmail] = userState('');
+    const [user_type,setType] = userState('student');**/
+    return(
+    <form >
+        <input 
+        type="email"
+        /**value={email}
+        onChange={(e) => setEmail(e.target.value)}**//>
+        <label class="form-label" >Email address</label>
+       
+       <br></br>
+        {/* <input type="password" id="Password"  />
+        <label >Password</label> */}
+        <label for="rank">Choose a login type:</label>
+            <select /**value={user_type} onChange={(e) => setEmail(e.target.value)}**/ >
+                <option value="admin">Admin</option>
+                <option value="student">Student</option>
+                <option value="lecturer">Lecturer</option>
+            </select>
+        <br></br>
+        <button >Send Password</button>
+    </form>)
+
+};
+export default Login_page;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import About from "./About";
+import Login_page from "./login"
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login_page />} />
           <Route path="about" element={<About />} />
         </Routes>
       </BrowserRouter>
