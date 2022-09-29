7a6b88c98dce3f86a2e2382eece48cdb72df5c88
Abinash niure <82077593+Abinash083@users.noreply.github.com>
2022-09-29 17:53:44 +0930

    Update login.jsx

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,37 +1,52 @@
-import {userState} from "react";
-import App from "./App";
+import {useState} from "react";
+import "./login.css"
 
 
 const Login_page = () => {
-    /**const [email,setEmail] = userState('');
-    const [user_type,setType] = userState('student');**/
+    const [email,setEmail] = useState('');
+    const [user_type,setType] = useState('student');
+    const [password,setPassword] = useState('');
     const handellogin = (e) =>{
         e.preventDefault();
-        console.log("login")
+        console.log(email)
         /**(pull data from php server to verify data)
          ***/
     }
 
     return(
-    <form onSubmit={handellogin}>
-        <input 
-        type="email"
-        /**value={email}
-        onChange={(e) => setEmail(e.target.value)}**//>
-        <label  >Email address</label>
-       
-       <br></br>
-        {/* <input type="password" id="Password"  />
-        <label >Password</label> */}
-        <label >Choose a login type:</label>
-            <select /**value={user_type} onChange={(e) => setType(e.target.value)}**/ >
+        <div>
+            
+            <form onSubmit={handellogin}>
+            
+            <label  >Email address : </label>
+            <br></br>
+            <input 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+            <br></br>
+            <label >Choose a login type:</label>
+            <br></br>
+            <select value={user_type} onChange={(e) => setType(e.target.value)} >
                 <option value="admin">Admin</option>
                 <option value="student">Student</option>
                 <option value="lecturer">Lecturer</option>
             </select>
-        <br></br>
-        <a href="/">Send Password</a>
-    </form>)
+            <br></br>
+            {user_type !== "student" && <label >Password:</label>}
+            <br></br>
+            {user_type !== "student" && <label >Password:</label> &&<input type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>}
+            
+            <br></br>
+            
+            <br></br>
+            {user_type === "student" && <button  type = "submit">Send Password</button>}
+            {user_type !== "student" && <button  >Login</button>}
+        </form>
+        </div>
+    )
 
 };
 export default Login_page;
